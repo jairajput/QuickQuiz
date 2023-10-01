9ff86504e6f298304b9b67d8d5f739b757ff7490
Jai <80166407+jairajput@users.noreply.github.com>
2023-10-02 04:53:44 +0530

    Initial Commit

---
+++ QuickQuiz/QuickQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickQuizApp.swift
+//  QuickQuiz
+//
+//  Created by Jai  on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
